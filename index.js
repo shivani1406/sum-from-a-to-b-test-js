6a11d77b0cfab7ef4757dc5efffc743c20d3d08f
Shivani Sharma <shivani1406sharma@gmail.com>
2021-12-28 15:49:36 +0000

    implemented recurrsion

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+
+  if (fromN < toN) {
+    return (sum(fromN + 1, toN) + fromN);
+
+  } else {
+    return (fromN);
+  }
+
 }
 
 module.exports = sum;
